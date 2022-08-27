d47ae1f50f885cac2a0356423a163fb38543e22f
BG Rodríguez <bengusrr@gmail.com>
2022-08-28 00:43:59 +0200

    20220828 update

--- Exercise-02.js
+++ Exercise-02.js
@@ -1,18 +1,19 @@
-const carta = 'bici coche balón _playstation bici coche peluche bici switch';
+const carta = ' bici coche  balón _playstation bici coche peluche bici switch ';
 
-export default function listGifts(letter) {
-  // ¡Tú puedes!
-  let words = [];
-  words = letter.split(" ");
+//export default 
+function listGifts(letter) {
 
-  console.log(words)
-  return {}
- }
+  const letterObjects = letter.trim().split(' ');
+  const count = {};
+  const resultado = letterObjects.filter( letterObjects => !letterObjects.includes('_') );
+
+  resultado.forEach( i => {
+    count[i] = ( count[i] || 0 ) + 1;
+  });
  
+  console.log(count);
+  return count;
+}
+  
+const regalos = listGifts(carta);
  
- // 1- crear función que recorra la cadena de regalos y extráiga en un array cada uno de los regalos
- // 1.1- Hay que controlar si hay un solo espacio que separa los regalos, o un guión bajo
- 
- // 2- crear objeto para almacenar el regalo y el número de veces que aparece
- 
- const regalos = listGifts(carta);
